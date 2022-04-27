6db5250c51f27393b2d8771aa6ad9b030f55637d
junuu <bababrll@naver.com>
2022-04-27 23:16:47 +0900

    #22 MemberService 구현체 생성

---
+++ Anthill/src/main/java/anthill/Anthill/service/MemberServiceImpl.java
@@ -0,0 +1,20 @@
+package anthill.Anthill.service;
+
+import anthill.Anthill.domain.Member;
+import anthill.Anthill.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberServiceImpl implements MemberService{
+
+    private MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public int regist(Member member) {
+        return 0;
+    }
+}
