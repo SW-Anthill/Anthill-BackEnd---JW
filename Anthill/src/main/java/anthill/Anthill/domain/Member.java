23059ce25e262402559fa0047eb1785fd7d3dba8
junuu <bababrll@naver.com>
2022-04-30 02:42:27 +0900

    @27 Member 클래스 user_id 필드값에 @NonNull 제약 추가

--- Anthill/src/main/java/anthill/Anthill/domain/Member.java
+++ Anthill/src/main/java/anthill/Anthill/domain/Member.java
@@ -1,14 +1,10 @@
 package anthill.Anthill.domain;
 
-
-
 import lombok.*;
 
 import javax.persistence.*;
 
-import static lombok.AccessLevel.PROTECTED;
-
-@NoArgsConstructor(access = PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 @Getter
@@ -18,10 +14,11 @@ public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //MySQL의 AUTO_INCREMENT를 사용
-    @Column(name="member_Id")
+    @Column(name = "member_id")
     private Long id;
 
-    @Column(name ="user_Id", nullable = false, unique = true, length = 20)
-    String userId;
+
+    @Column(name = "user_id", nullable = false, unique = true, length = 20)
+    @NonNull String userId;
 
 }
