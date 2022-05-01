06b00ee2ed380df2952ec31675ec30d578154089
junuu <bababrll@naver.com>
2022-05-01 16:51:59 +0900

    #28 MemberRepository에 findByUserId 메서드 추가

--- Anthill/src/main/java/anthill/Anthill/repository/MemberRepository.java
+++ Anthill/src/main/java/anthill/Anthill/repository/MemberRepository.java
@@ -1,7 +1,8 @@
 package anthill.Anthill.repository;
 
-import anthill.Anthill.domain.Member;
+import anthill.Anthill.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByUserId(String userId);
 }
