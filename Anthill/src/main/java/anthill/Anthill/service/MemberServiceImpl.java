a43656fac8ec13da41d9afe53d94c5e04e46eee6
junuu <bababrll@naver.com>
2022-05-03 01:43:37 +0900

    #43 MemberServiceImpl 메서드 추가 - 유효성 검증을 위한 메서드들 추가

--- Anthill/src/main/java/anthill/Anthill/service/MemberServiceImpl.java
+++ Anthill/src/main/java/anthill/Anthill/service/MemberServiceImpl.java
@@ -4,6 +4,8 @@ import anthill.Anthill.domain.member.Member;
 import anthill.Anthill.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class MemberServiceImpl implements MemberService{
 
@@ -14,7 +16,25 @@ public class MemberServiceImpl implements MemberService{
     }
 
     @Override
-    public int join(Member member) {
+    public int join(Member member){
+        memberRepository.save(member);
         return 0;
     }
+
+    @Override
+    public boolean checkNicknameDuplicate(String nickName) {
+        return memberRepository.existsByNickName(nickName);
+    }
+
+    @Override
+    public boolean checkUserIdDuplicate(String userId) {
+        return memberRepository.existsByUserId(userId);
+    }
+
+    @Override
+    public boolean checkPhoneNumberDuplicate(String phoneNumber) {
+        return memberRepository.existsByPhoneNumber(phoneNumber);
+    }
+
+
 }
