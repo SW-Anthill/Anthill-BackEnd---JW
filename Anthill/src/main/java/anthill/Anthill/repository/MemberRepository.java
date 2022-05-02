08bf3994218d778f64b3096eaf11cb6d51044bd4
junuu <bababrll@naver.com>
2022-05-03 01:42:32 +0900

    #41 MemberRepository에 메서드 3개 추가  [existedBy(UserId, nickName,phoneNumber) ]

--- Anthill/src/main/java/anthill/Anthill/repository/MemberRepository.java
+++ Anthill/src/main/java/anthill/Anthill/repository/MemberRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findByUserId(String userId);
+    boolean existsByUserId(String userId);
+    boolean existsByNickName(String nickName);
+    boolean existsByPhoneNumber(String phoneNumber);
 }
