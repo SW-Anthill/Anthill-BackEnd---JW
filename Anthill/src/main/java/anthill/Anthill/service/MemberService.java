ea3895ba96c9a2d209127e8e7f7b0cdaef8598bc
junuu <bababrll@naver.com>
2022-05-03 01:43:07 +0900

    #42 MemberService interface 수정 유효성 검증을 위한 existsBy Method들 추가

--- Anthill/src/main/java/anthill/Anthill/service/MemberService.java
+++ Anthill/src/main/java/anthill/Anthill/service/MemberService.java
@@ -4,5 +4,7 @@ import anthill.Anthill.domain.member.Member;
 
 public interface MemberService {
     public int join(Member member);
-
+    boolean checkNicknameDuplicate(String nickName);
+    boolean checkUserIdDuplicate(String userId);
+    boolean checkPhoneNumberDuplicate(String phoneNumber);
 }
