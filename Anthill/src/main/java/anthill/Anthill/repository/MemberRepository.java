b1c64c0271011db5e0e937263d17200b9355f096
junuu <bababrll@naver.com>
2022-05-06 22:15:02 +0900

    #57 MemberRepository 수정 : findByUserId 반환값 Optional으로 변경

--- Anthill/src/main/java/anthill/Anthill/repository/MemberRepository.java
+++ Anthill/src/main/java/anthill/Anthill/repository/MemberRepository.java
@@ -3,8 +3,10 @@ package anthill.Anthill.repository;
 import anthill.Anthill.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByUserId(String userId);
+    Optional<Member> findByUserId(String userId);
     boolean existsByUserId(String userId);
     boolean existsByNickName(String nickName);
     boolean existsByPhoneNumber(String phoneNumber);
