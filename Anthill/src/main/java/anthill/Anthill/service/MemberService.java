b18b66923945494c12fa24e083706a2934d8b285
junuu <bababrll@naver.com>
2022-05-07 23:13:47 +0900

    #60 MemberService 수정 : validateIsDuplicate 메서드 추가

--- Anthill/src/main/java/anthill/Anthill/service/MemberService.java
+++ Anthill/src/main/java/anthill/Anthill/service/MemberService.java
@@ -1,9 +1,12 @@
 package anthill.Anthill.service;
 
 import anthill.Anthill.domain.member.Member;
+import anthill.Anthill.dto.member.MemberRequestDTO;
+import anthill.Anthill.dto.member.MemberResponseDTO;
 
 public interface MemberService {
-    public int join(Member member);
+    void join(Member member);
+    boolean validateIsDuplicate(MemberRequestDTO memberRequestDTO);
     boolean checkNicknameDuplicate(String nickName);
     boolean checkUserIdDuplicate(String userId);
     boolean checkPhoneNumberDuplicate(String phoneNumber);
