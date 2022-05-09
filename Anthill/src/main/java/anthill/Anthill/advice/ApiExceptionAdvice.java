1819630acd78bae4719e1ba41a78a50ca82f7e49
junuu <bababrll@naver.com>
2022-05-09 22:47:00 +0900

    #62  ApiExceptionAdvice 수정 : 데이터 중복 에러 추가

--- Anthill/src/main/java/anthill/Anthill/advice/ApiExceptionAdvice.java
+++ Anthill/src/main/java/anthill/Anthill/advice/ApiExceptionAdvice.java
@@ -1,5 +1,6 @@
 package anthill.Anthill.advice;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,8 +10,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice(annotations = RestController.class)
 public class ApiExceptionAdvice {
 
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> dataBaseExceptionHandler(DataIntegrityViolationException e){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("데이터 중복 에러");
+    }
+
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> exceptionHandler(Exception e){
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    public ResponseEntity<String> internalServerExceptionHandler(Exception e){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 내부 에러");
     }
 }
