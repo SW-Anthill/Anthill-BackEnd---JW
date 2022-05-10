40ce692b7d69189cd9ef25be7fe1b466c10a36db
junuu <bababrll@naver.com>
2022-05-10 21:36:33 +0900

    #65 MemberRequestDTO 수정: 에러 해결을 위한 빈 생성자 및 모든 필드 생성자 추가

--- Anthill/src/main/java/anthill/Anthill/dto/member/MemberRequestDTO.java
+++ Anthill/src/main/java/anthill/Anthill/dto/member/MemberRequestDTO.java
@@ -2,13 +2,14 @@ package anthill.Anthill.dto.member;
 
 import anthill.Anthill.domain.member.Address;
 import anthill.Anthill.domain.member.Member;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 @Getter
 public class MemberRequestDTO {
@@ -18,15 +19,15 @@ public class MemberRequestDTO {
     //@Pattern : 지정된 패턴만 입력하게 하여 휴대폰 번호 폼에서 이상한 값들이 요청되는 것을 방지합니다.
 
     @NotBlank(message = "아이디를 입력해주세요.")
-    @Size(min=5, max=20, message="아이디는 5자 이상 20자 이하로 입력해주세요.")
+    @Size(min = 5, max = 20, message = "아이디는 5자 이상 20자 이하로 입력해주세요.")
     private String userId;
 
     @NotBlank(message = "비밀번호를 입력해주세요.")
-    @Size(min=8, message="비밀번호를 8자 이상으로 입력해주세요.")
+    @Size(min = 8, message = "비밀번호를 8자 이상으로 입력해주세요.")
     private String password;
 
     @NotBlank(message = "별명을 입력해주세요.")
-    @Size(max=20, message="별명을 20자 이하로 입력해주세요.")
+    @Size(max = 20, message = "별명을 20자 이하로 입력해주세요.")
     private String nickName;
 
     @NotBlank(message = "이름을 입력해주세요.")
@@ -38,7 +39,7 @@ public class MemberRequestDTO {
 
     Address address;
 
-    public Member toEntity(){
+    public Member toEntity() {
         return Member.builder().userId(userId).password(password).nickName(nickName).name(name).phoneNumber(phoneNumber).address(address).build();
     }
 
