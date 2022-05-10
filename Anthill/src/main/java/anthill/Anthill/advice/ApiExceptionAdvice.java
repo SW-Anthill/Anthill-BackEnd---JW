90b8bdb997aeb09422371f7c7c0ae6da081166e3
junuu <bababrll@naver.com>
2022-05-10 21:35:22 +0900

    #63 ApiExceptionAdvice 수정 : 데이터 유효성 예외처리를 위한 메서드 추가

--- Anthill/src/main/java/anthill/Anthill/advice/ApiExceptionAdvice.java
+++ Anthill/src/main/java/anthill/Anthill/advice/ApiExceptionAdvice.java
@@ -1,23 +1,34 @@
 package anthill.Anthill.advice;
 
+
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+
 @RestControllerAdvice(annotations = RestController.class)
 public class ApiExceptionAdvice {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> dataInvalidateExceptionHandler(MethodArgumentNotValidException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("입력 데이터가 유효하지 않습니다.");
+    }
+
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<String> dataBaseExceptionHandler(DataIntegrityViolationException e){
+    public ResponseEntity<String> dataBaseExceptionHandler(DataIntegrityViolationException e) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body("데이터 중복 에러");
     }
 
-
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> internalServerExceptionHandler(Exception e){
+    public ResponseEntity<String> internalServerExceptionHandler(Exception e) {
+        System.out.println(e.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 내부 에러");
     }
+
+
 }
