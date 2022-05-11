aad6f0701ac3734ce3aa7b351aee7a05329f873e
junuu <bababrll@naver.com>
2022-05-12 03:03:52 +0900

    #68 MemberControllerTest 테스트 추가 : memberRequestDTO의 입력값들이 유효하지 않을때 테스트 추가

--- Anthill/src/test/java/anthill/Anthill/controller/MemberControllerTest.java
+++ Anthill/src/test/java/anthill/Anthill/controller/MemberControllerTest.java
@@ -1,14 +1,16 @@
 package anthill.Anthill.controller;
 
+import anthill.Anthill.dto.member.MemberRequestDTO;
+import anthill.Anthill.service.MemberService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -21,6 +23,9 @@ class MemberControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private MemberService memberService;
+
     @Test
     @DisplayName("Hello Test")
     public void returnOkMessage() throws Exception {
@@ -28,11 +33,29 @@ class MemberControllerTest {
         String ok = "ok";
 
         //when
-
-        //then
         mvc.perform(get("/members"))
+                //then
                 .andExpect(status().isOk())
                 .andExpect(content().string(ok));
     }
+
+    @Test
+    @DisplayName("memberRequestDTO의 입력값들이 유효하지 않을때")
+    public void memberPostDataInValidateTest() throws Exception {
+        //given
+        MemberRequestDTO memberRequestDTO = MemberRequestDTO.builder().build();
+        String body = (new ObjectMapper()).writeValueAsString(memberRequestDTO);
+
+        //when
+        mvc.perform(post("/members")
+                        .content(body)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                )
+                //then
+                .andExpect(status().isBadRequest());
+    }
+
+
 
 }
