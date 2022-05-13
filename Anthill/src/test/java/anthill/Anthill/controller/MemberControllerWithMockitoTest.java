9bc292a81d182614463e7f3114190d88eb7b003a
junuu <bababrll@naver.com>
2022-05-13 15:08:14 +0900

    #74 MemberControllerWithMockitoTest 생성 : MockitoFramework + JUnit5를 활용한 Controller 테스트

---
+++ Anthill/src/test/java/anthill/Anthill/controller/MemberControllerWithMockitoTest.java
@@ -0,0 +1,50 @@
+package anthill.Anthill.controller;
+
+import anthill.Anthill.service.MemberService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+public class MemberControllerWithMockitoTest {
+
+    @InjectMocks
+    private MemberController memberController;
+
+    @Mock
+    private MemberService memberService;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void init() {
+        mockMvc = MockMvcBuilders.standaloneSetup(memberController).build();
+    }
+
+    @Test
+    @DisplayName("Hello Test")
+    public void returnOkMessage() throws Exception {
+        //given
+        String ok = "ok";
+
+        //when
+        ResultActions resultActions= mockMvc.perform(get("/members"));
+
+        //then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(content().string(ok));
+    }
+
+}
