e0405cf7a66492cc8edcca0c592215d8a9e55c68
junuu <bababrll@naver.com>
2022-05-23 23:28:24 +0900

    #93 MemberRequestDTOTest 생성 : 비밀번호 해싱화 테스트

---
+++ Anthill/src/test/java/anthill/Anthill/dto/member/MemberRequestDTOTest.java
@@ -0,0 +1,52 @@
+package anthill.Anthill.dto.member;
+
+import anthill.Anthill.domain.member.Address;
+import anthill.Anthill.domain.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRequestDTOTest {
+
+    @Test
+    @DisplayName("회원 비밀번호 해싱화")
+    public void memberPasswordHashingTest(){
+        //given
+        String originPassword = "Test";
+        MemberRequestDTO memberRequestDTO = MemberRequestDTO.builder()
+                                                            .userId("Test")
+                                                            .name("Test")
+                                                            .nickName("Test")
+                                                            .password(originPassword)
+                                                            .phoneNumber("Test")
+                                                            .address(new Address("a1", "a2", "a3"))
+                                                            .build();
+        //when
+        memberRequestDTO.hashingPassword();
+
+        //then
+        Assertions.assertThat(originPassword).isNotEqualTo(memberRequestDTO.getPassword());
+    }
+
+    @Test
+    @DisplayName("DTO를 Entity로 변경하는 테스트")
+    public void memberRequestDtoToEntityTest(){
+        //given
+        MemberRequestDTO memberRequestDTO = MemberRequestDTO.builder()
+                                                            .userId("Test")
+                                                            .name("Test")
+                                                            .nickName("Test")
+                                                            .password("Test")
+                                                            .phoneNumber("Test")
+                                                            .address(new Address("a1", "a2", "a3"))
+                                                            .build();
+        //when
+        Member member = memberRequestDTO.toEntity();
+
+        //then
+        Assertions.assertThat(member).isInstanceOf(Member.class);
+    }
+
+}
