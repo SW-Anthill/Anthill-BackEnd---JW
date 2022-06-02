ce8d5a2bfad173ec7bb2a6c921d41b596217139a
junuu <bababrll@naver.com>
2022-06-02 23:44:55 +0900

    #97 효율적인 단위 테스트를 위해 WebMvcTest에 테스트할 class 붙여주기

--- Anthill/src/test/java/anthill/Anthill/controller/MemberControllerTest.java
+++ Anthill/src/test/java/anthill/Anthill/controller/MemberControllerTest.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 
-@WebMvcTest
+@WebMvcTest(MemberController.class)
 class MemberControllerTest {
 
     @Autowired
