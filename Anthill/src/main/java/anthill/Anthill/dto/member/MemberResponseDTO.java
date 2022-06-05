85294094571e5246ddb39bee833a5532468beb79
junuu <bababrll@naver.com>
2022-06-06 04:28:03 +0900

    #106 MemberResponseDTO 생성 : 회원 조회 후 데이터 반환을 위한 DTO 생성

--- Anthill/src/main/java/anthill/Anthill/dto/member/MemberResponseDTO.java
+++ Anthill/src/main/java/anthill/Anthill/dto/member/MemberResponseDTO.java
@@ -1,5 +1,23 @@
 package anthill.Anthill.dto.member;
 
-public class MemberResponseDTO {
+import anthill.Anthill.domain.member.Address;
+import lombok.Builder;
+import lombok.Getter;
 
+@Getter
+public class MemberResponseDTO {
+    private String userId;
+    private String nickName;
+    private String name;
+    private String phoneNumber;
+    Address address;
+
+    @Builder
+    public MemberResponseDTO(String userId, String nickName, String name, String phoneNumber, Address address) {
+        this.userId = userId;
+        this.nickName = nickName;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
 }
