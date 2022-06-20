e1889d13242c9bb0eeace593d8f63e8f81654e2d
junuu <bababrll@naver.com>
2022-06-20 20:36:08 +0900

    fix : ObjectMapper를 위한 Getter 추가
    Address DTO에는 @Getter가 없었습니다.
    따라서 ObjectMapper를 사용하여 JSON으로 변환이 불가능하였으며 @Getter를 추가함으로써 JSON으로 변환이 가능해졌습니다.

--- Anthill/src/main/java/anthill/Anthill/domain/member/Address.java
+++ Anthill/src/main/java/anthill/Anthill/domain/member/Address.java
@@ -1,16 +1,24 @@
 package anthill.Anthill.domain.member;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
-@AllArgsConstructor
 @NoArgsConstructor
-public class Address {
+@Getter
+public class Address{
     private String zipCode;
     private String address1;
     private String address2;
 
+    @Builder
+    public Address(String zipCode, String address1, String address2) {
+        this.zipCode = zipCode;
+        this.address1 = address1;
+        this.address2 = address2;
+    }
 }
