eef1928cfb9f90e8ce7144a227eafb62920420b4
junuu <bababrll@naver.com>
2022-06-23 22:20:35 +0900

    feat : 게시글 엔티티 생성
    @ManyToOne 어노테이션을 이용하여 양방향 연관관계의 외래키를 관리
    setMember, deleteMember등 연관관계 편의메서드 추가
    updateBoard 변경감지를 위한 메서드 추가

---
+++ Anthill/src/main/java/anthill/Anthill/domain/board/Board.java
@@ -0,0 +1,70 @@
+package anthill.Anthill.domain.board;
+
+import anthill.Anthill.domain.member.Member;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Table(name = "board")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicUpdate
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_id")
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "writer", nullable = false, length = 20)
+    private String writer;
+
+    @Column(name = "hits")
+    private Long hits;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Builder
+    public Board(Long id, String title, String content, String writer, Long hits) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.hits = hits;
+    }
+
+    public void setMember(Member member) {
+        if (this.member != null) {
+            this.member.getBoards()
+                       .remove(this);
+        }
+        this.member = member;
+        member.getBoards()
+              .add(this);
+    }
+
+    public void updateBoard(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void deleteMember() {
+        if (this.member != null) {
+            this.member.getBoards()
+                       .remove(this);
+        }
+
+    }
+}
