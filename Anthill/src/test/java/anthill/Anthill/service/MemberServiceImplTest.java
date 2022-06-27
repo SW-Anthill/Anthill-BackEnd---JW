6194342b75db67568c9b30af2895059cfd041cb5
junuu <bababrll@naver.com>
2022-06-27 17:31:42 +0900

    refactor : 사용하지 않는 import 제거
    NosuchElementException
    Optional
    제거

--- Anthill/src/test/java/anthill/Anthill/service/MemberServiceImplTest.java
+++ Anthill/src/test/java/anthill/Anthill/service/MemberServiceImplTest.java
@@ -13,9 +13,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.NoSuchElementException;
-import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
