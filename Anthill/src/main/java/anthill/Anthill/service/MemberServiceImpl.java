840a5a913910b4f6d12190a0d18487909298d89f
junuu <bababrll@naver.com>
2022-07-01 03:48:57 +0900

    feat : @Transactional 어노테이션 추가

--- Anthill/src/main/java/anthill/Anthill/service/MemberServiceImpl.java
+++ Anthill/src/main/java/anthill/Anthill/service/MemberServiceImpl.java
@@ -8,11 +8,13 @@ import anthill.Anthill.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
